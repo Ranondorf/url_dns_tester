fc3e99260a4c44c05dda3299b2904e81bc944e87
Rui <rui7@tuta.io>
2024-07-19 13:49:17 +1000

    Adjust curl command

--- url_dns_tester.sh
+++ url_dns_tester.sh
@@ -15,12 +15,13 @@ do
 	echo "#########Entry: $entry ###" >> $output_file
 	echo "###################################" >> $output_file
         echo "--------------Date-----------------" >> $output_file
-	date >> $output_file
+	date -u >> $output_file
 	echo "--------------Dig------------------" >> $output_file
 	dig $1 @8.8.8.8 >> $output_file
 	dig $1 >> $output_file
 	echo "---------Curl--Result--------------" >> $output_file
-	curl -I -X GET https://$1/$2 | grep 'HTTP' >> $output_file
+	# curl -I -X GET https://$1/$2 | grep 'HTTP' >> $output_file
+        curl -k -s -o /dev/null --connect-timeout 2 -w "%{remote_ip} - %{http_code}\n" https://$1/$2 >> $output_file
 	echo "###################################" >> $output_file
 	echo >> $output_file
 	entry=$((++entry))
